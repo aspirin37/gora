1ab7e9659dba862eccd01c3515a84e0fda1e0d82
aspirin37 <molir10@mail.ru>
2018-05-24 15:02:44 +0300

    MST-3334 main page

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,54 +1,76 @@
 <template>
   <div>
-    <article class="py-5 text-center height-full d-flex align-items-center js-scroll-block" id="main">
+    <article class="py-5 text-center height-full d-flex align-items-center js-scroll-block current-shadow" id="main">
       <div class="w-100 mb-5 container">
         <h1 class="text-center mw-800 mx-auto">Best mobile & web applications all over<span class="title-char"></span></h1>
-        <p class="text-center mw-600 mx-auto mb-5">We are «A3 technology» — mobile and web development company focused on modern technologies and knowledge related on delivery, blockchain, e-commerce.</p>
+        <p class="text-center mw-600 mx-auto mb-5">We are «GORA» — mobile and web development company focused on modern technologies and knowledge related on delivery, blockchain, e-commerce.</p>
         <button class="btn btn-pink btn-lg btn-rounded">Keep in touch</button>
       </div>
     </article>
 
-    <article class="bg-light py-5">
+    <article class="bg-light main-section js-scroll-block relative current-shadow" id="services">
       <div class="container mb-5">
-        <h3>What we do<span class="title-char"></span></h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam sequi quam ipsa amet facere veritatis exercitationem neque et doloremque possimus id velit accusamus laboriosam at optio ratione, non corporis ad!</p>
+        <h3 class="section-title">What we do<span class="title-char"></span></h3>
+        <p class="font-large">Мы занимаемся разработкой средних и крупных проектов под все популярные платформы: iOS, Android, Web. Основные направления нашей деятельности это разработка мобильных приложений и крупных порталов.</p>
       </div>
       <div class="container-fluid">
-        <div class="row align-items-center">
+        <div class="row align-items-center py-5">
           <div class="col-12 col-md-6 text-right">
-            img
+            <img src="@/images/mobile-app.png" alt="mobile" class="mw-100 service-pic">
           </div>
           <div class="col-12 col-md-6">
-            <p class="mw-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas accusantium aliquid sit minima, perferendis excepturi cupiditate, perspiciatis amet itaque consequuntur ipsa sequi. Sed ex nihil tempora enim earum delectus tenetur?</p>
+            <p class="font-large mw-600">Создаем мобильные решения, позволяющие повышать лояльность клиентов и снижать расходы за счет внедрения средств автоматизации и оптимизации процессов.</p>
           </div>
         </div>
-        <div class="row align-items-center">
+        <div class="row align-items-center py-5">
           <div class="col-12 col-md-6 text-right">
-            <p class="mw-600 ml-auto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas accusantium aliquid sit minima, perferendis excepturi cupiditate, perspiciatis amet itaque consequuntur ipsa sequi. Sed ex nihil tempora enim earum delectus tenetur?</p>
+            <p class="font-large mw-600 ml-auto">Проектируем архитектуру проекта индивидуально для каждого кейса, учитывая все возможные платформы. Все для того чтобы создать по-настоящему качественный продукт.</p>
           </div>
           <div class="col-12 col-md-6">
-            img
+            <img src="@/images/web-app.png" alt="web" class="mw-100 service-pic">
           </div>
         </div>
       </div>
     </article>
 
-    <section class="height-full project-section container-fluid align-items-center js-scroll-block d-flex justify-content-center flex-column" id="project-1">
+    <section class="height-full project-section container-fluid js-scroll-block d-flex justify-content-center flex-column" id="project-1">
+      <div class="container">
+        <h3 class="section-title">Lasets jobs<span class="title-char"></span></h3>
+      </div>
       <div class="row py-4 project-section__info">
-        <div class="col-12 col-lg-3 text-center">
+        <div class="col-12 col-lg-4 text-center">
           <a href="#" class="dl-link rounded mx-2 p-3"><img class="dl-link__img" src="@/images/android.svg" alt="android"></a>
           <a href="#" class="dl-link rounded mx-2 p-3"><img class="dl-link__img" src="@/images/apple.svg" alt="apple"></a>
           <a href="#" class="dl-link rounded mx-2 p-3"><img class="dl-link__img" src="@/images/web.svg" alt="web"></a>
         </div>
-        <div class="col-12 col-lg-4"></div>
+        <div class="col-12 col-lg-3"></div>
         <div class="col-12 col-lg-4">
-          <h3 class="h1">Mesto.Delivery</h3>
-          <p>Место — сервис умных доставок день в день по Санкт-Петербургу от 250 руб. Подача 60 мин. Следи за посылкой онлайн. Личный кабинет. Простые iOS и Android приложения. Оплата наличными и картой. Работаем с юр. лицами. Аренда курьера на день.</p>
+          <h3 class="h1 mb-4">Mesto.Delivery</h3>
+          <p class="font-large">Место — сервис умных доставок день в день по Санкт-Петербургу от 250 руб. Подача 60 мин. Следи за посылкой онлайн. Личный кабинет. Простые iOS и Android приложения. Оплата наличными и картой. Работаем с юр. лицами. Аренда курьера на день.</p>
+          <a href="#">Read more</a>
         </div>
       </div>
       <div class="project-section__clip">
         <div class="project-section__item mb-4 mb-md-0 mx-auto">
-          <img src="@/images/iphone.svg" alt="" class="mw-100 project-section__frame">
+          <img src="@/images/samsung.svg" alt="" class="mw-100 project-section__frame">
+          <img src="@/images/projects/mesto-phone.png" alt="" class="project-section__pic">
+        </div>
+      </div>
+    </section>
+
+    <section class="height-full project-section container-fluid align-items-center js-scroll-block d-flex justify-content-center flex-column">
+      <div class="row py-4 project-section__info">
+        <div class="col-12 col-lg-4 text-right">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum laborum odit aspernatur dignissimos magnam dolor accusantium veritatis tempora suscipit. Ea vel dignissimos inventore saepe enim molestias aliquid in similique quo.</p>
+        </div>
+        <div class="col-12 col-lg-3"></div>
+        <div class="col-12 col-lg-4">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum laborum odit aspernatur dignissimos magnam dolor accusantium veritatis tempora suscipit. Ea vel dignissimos inventore saepe enim molestias aliquid in similique quo.</p>
+        </div>
+      </div>
+      <div class="project-section__clip">
+        <div class="project-section__item mb-4 mb-md-0 mx-auto">
+          <img src="@/images/samsung.svg" alt="" class="mw-100 project-section__frame">
           <img src="@/images/projects/mesto-phone.png" alt="" class="project-section__pic">
         </div>
       </div>
@@ -56,15 +78,34 @@
 
     <section class="height-full project-section container-fluid align-items-center js-scroll-block d-flex justify-content-center flex-column" id="project-2">
       <div class="row py-4 project-section__info">
-        <div class="col-12 col-lg-3 text-center">
-          <a href="#" class="dl-link rounded mx-2 p-3"><img class="dl-link__img" src="@/images/android.svg" alt="android"></a>
-          <a href="#" class="dl-link rounded mx-2 p-3"><img class="dl-link__img" src="@/images/apple.svg" alt="apple"></a>
-          <a href="#" class="dl-link rounded mx-2 p-3"><img class="dl-link__img" src="@/images/web.svg" alt="web"></a>
+        <div class="col-12 col-lg-4 text-center">
+          <a href="https://linger.im/" class="dl-link rounded mx-2 p-3"><img class="dl-link__img" src="@/images/web.svg" alt="web"></a>
         </div>
-        <div class="col-12 col-lg-4"></div>
+        <div class="col-12 col-lg-3"></div>
         <div class="col-12 col-lg-4">
-          <h3>Web Projects</h3>
-          <p>Проектируем архитектуру проекта индивидуально для каждого кейса, учитывая все возможные платформы. Все для того чтобы создать по-настоящему качественный продукт.</p>
+          <h3 class="h1 mb-4">Linger</h3>
+          <p class="font-large">Место — сервис умных доставок день в день по Санкт-Петербургу от 250 руб. Подача 60 мин. Следи за посылкой онлайн. Личный кабинет. Простые iOS и Android приложения. Оплата наличными и картой. Работаем с юр. лицами. Аренда курьера на день.</p>
+          <a href="#">Read more</a>
+        </div>
+      </div>
+      <div class="project-section__clip">
+        <div class="project-section__item mb-4 mb-md-0 mx-auto">
+          <img src="@/images/iphone.svg" alt="" class="mw-100 project-section__frame">
+          <img src="@/images/projects/linger.png" alt="" class="project-section__pic">
+        </div>
+      </div>
+    </section>
+
+    <section class="height-full project-section container-fluid align-items-center js-scroll-block d-flex justify-content-center flex-column" id="project-3">
+      <div class="row py-4 project-section__info">
+        <div class="col-12 col-lg-4 text-center">
+          <a href="#" class="dl-link rounded mx-2 p-3"><img class="dl-link__img" src="@/images/android.svg" alt="android"></a>
+        </div>
+        <div class="col-12 col-lg-3"></div>
+        <div class="col-12 col-lg-4">
+          <h3 class="h1 mb-4">KUC</h3>
+          <p class="font-large">Место — сервис умных доставок день в день по Санкт-Петербургу от 250 руб. Подача 60 мин. Следи за посылкой онлайн. Личный кабинет. Простые iOS и Android приложения. Оплата наличными и картой. Работаем с юр. лицами. Аренда курьера на день.</p>
+          <a href="#">Read more</a>
         </div>
       </div>
       <div class="project-section__clip">
@@ -75,71 +116,32 @@
       </div>
     </section>
 
-    <section class="height-full project-section container-fluid align-items-center js-scroll-block d-flex justify-content-center flex-column" id="project-3">
-      <div class="row py-4 project-section__info">
-        <div class="col-12 col-lg-3 text-center">
-          <a href="#" class="dl-link rounded mx-2 p-3"><img class="dl-link__img" src="@/images/android.svg" alt="android"></a>
-          <a href="#" class="dl-link rounded mx-2 p-3"><img class="dl-link__img" src="@/images/apple.svg" alt="apple"></a>
-          <a href="#" class="dl-link rounded mx-2 p-3"><img class="dl-link__img" src="@/images/web.svg" alt="web"></a>
-        </div>
-        <div class="col-12 col-lg-4"></div>
-        <div class="col-12 col-lg-4">
-          <h3>Blockchain</h3>
-          <p>Консультируем и разрабатываем проекты с использованием Blockchain технологий. Рассчет токеномики, сопровождение ICO, экспертиза цифровых активов, внедрение FOMO-подхода. Будьте в тренде цифровой трансформации!</p>
-        </div>
-      </div>
-      <div class="project-section__clip">
-        <div class="project-section__item">
-          <img src="@/images/iphone.svg" alt="" class="mw-100">
-        </div>
-      </div>
-    </section>
-
-    <section class="height-full project-section container-fluid align-items-center js-scroll-block d-flex justify-content-center flex-column" id="project-4">
-      <div class="row py-4 project-section__info">
-        <div class="col-12 col-lg-3 text-center">
-          <a href="#" class="dl-link rounded mx-2 p-3"><img class="dl-link__img" src="@/images/android.svg" alt="android"></a>
-          <a href="#" class="dl-link rounded mx-2 p-3"><img class="dl-link__img" src="@/images/apple.svg" alt="apple"></a>
-          <a href="#" class="dl-link rounded mx-2 p-3"><img class="dl-link__img" src="@/images/web.svg" alt="web"></a>
-        </div>
-        <div class="col-12 col-lg-4"></div>
-        <div class="col-12 col-lg-4">
-          <h3>E-Commerce</h3>
-          <p>Разрабатываем полноценные цифровые каналы продаж, интегрированные и автоматизированные решения обеспечивающие бесшовный покупательский опыт в рамках омниканальной стратегии.</p>
-        </div>
-      </div>
-      <div class="project-section__clip">
-        <div class="project-section__item">
-          <img src="@/images/samsung.svg" alt="" class="mw-100">
-        </div>
-      </div>
-    </section>
     <nav class="side-nav">
       <a href="#main"
         class="side-nav__link current-shadow d-block ml-auto"
         :class="{'bg-dark active': scrolledBlock === 'main'}"
         v-on:click.prevent="scrollToItem('main', 500)"
       ></a>
+      <a href="#services"
+        class="side-nav__link current-shadow d-block ml-auto"
+        :class="{'bg-dark active': scrolledBlock === 'services'}"
+        v-on:click.prevent="scrollToItem('services', 500)"
+      ><span class="side-nav__text">Услуги</span></a>
       <a href="#project-1"
         class="side-nav__link current-shadow d-block ml-auto"
         :class="{'bg-dark active': scrolledBlock === 'project-1'}"
         v-on:click.prevent="scrollToItem('project-1', 500)"
-      ><span class="side-nav__text">Mobile Apps</span></a>
+      ><span class="side-nav__text">Mesto.Delivery</span></a>
       <a href="#project-2"
         class="side-nav__link current-shadow d-block ml-auto"
         :class="{'bg-dark active': scrolledBlock === 'project-2'}"
         v-on:click.prevent="scrollToItem('project-2', 500)"
-      ><span class="side-nav__text">Web Projects</span></a>
+      ><span class="side-nav__text">Linger</span></a>
       <a href="#project-3"
         class="side-nav__link current-shadow d-block ml-auto"
         :class="{'bg-dark active': scrolledBlock === 'project-3'}"
         v-on:click.prevent="scrollToItem('project-3', 500)"
-      ><span class="side-nav__text">Blockchain</span></a>
-      <a href="#project-4"
-        class="side-nav__link current-shadow d-block ml-auto"
-        :class="{'bg-dark active': scrolledBlock === 'project-4'}"
-        v-on:click.prevent="scrollToItem('project-4', 500)"
-      ><span class="side-nav__text">E-Commerce</span></a>
+      ><span class="side-nav__text">KUC</span></a>
     </nav>
   </div>
 </template>
