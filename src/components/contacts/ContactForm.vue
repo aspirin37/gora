96e5ddb0ebebba8d263f55767713f0a55d7ff229
aspirin37 <molir10@mail.ru>
2018-06-14 17:35:56 +0300

    MST-3312 mailer

--- src/components/contacts/ContactForm.vue
+++ src/components/contacts/ContactForm.vue
@@ -1,15 +1,16 @@
 <template>
   <form v-on:submit.prevent="sendFeedback">
     <div class="form-group">
-      <input type="text" class="form-control" :placeholder="$t('titles.name')">
+      <input type="text" class="form-control" :placeholder="$t('titles.name')" v-model="feedName" required>
     </div>
     <div class="form-group">
-      <input type="text" class="form-control" :placeholder="$t('titles.phone')">
+      <input type="text" class="form-control" :placeholder="$t('titles.phone')" v-model="feedPhone" required>
     </div>
     <div class="form-group">
-      <textarea class="form-control" :placeholder="$t('titles.message')"></textarea>
+      <textarea class="form-control" :placeholder="$t('titles.message')" v-model="feedMessage"></textarea>
     </div>
-    <button type="submit" class="btn btn-theme w-100">{{$t('titles.send')}}</button>
+    <button type="submit" class="btn btn-theme w-100 mb-3">{{$t('titles.send')}}</button>
+    <span class="text-danger" v-if="errorMessage">{{errorMessage}}</span>
   </form>
 </template>
 
@@ -17,11 +18,27 @@
 export default {
   data () {
     return {
+      feedName: '',
+      feedPhone: '',
+      feedMessage: '',
+      errorMessage: ''
     }
   },
   methods: {
     sendFeedback () {
-      this.$emit('sended', true)
+      let options = {
+        phone: this.feedPhone,
+        name: this.feedName
+      }
+      if (this.feedMessage) {
+        options.message = this.feedMessage
+      }
+      this.$http.post('https://gora.studio/mailer/mail.php', options).then(response => {
+        this.$emit('sended', true)
+      }).catch(error => {
+        this.errorMessage = error
+      })
+      
     }
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VueResource from 'vue-resource'
 
+Vue.use(VueResource)
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
