3da7199b0ce07383d6e4f54e58f3c7c8180d2a98
aspirin37 <molir10@mail.ru>
2018-07-23 11:59:43 +0000

    de.js edited online with Bitbucket

--- src/store/languages/de.js
+++ src/store/languages/de.js
@@ -43,7 +43,7 @@ const site = {
     "address": "Bolshoy prospect PS, 29",
     "city": "Saint-Petersburg",
     "country": "Russland",
-    "phone": "+7 (812) 408-25-47",
+    "phone": "+7 (495) 131-0856",
     "sendedtitle": "Nachricht gesendet",
     "sendedsubtitle": "Wir werden uns bald bei Ihnen melden"
   },
